270bf4e4bceeade3b0866b6baa46f0734869281b
Ivan Kiiashko <89341731+MrQuinlan@users.noreply.github.com>
2022-01-28 23:20:00 +0200

    1st-2nd-tsks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('div.gallery');
+
+const makeGalleryMarkup = elements => {
+    return elements.map(
+        element =>
+            `<a class="gallery__link" href="${element.original}"><img class="gallery__image" src="${element.preview}" alt="${element.description}"/></a>`,
+    );
+};
+
+const galleryWrapper = makeGalleryMarkup(galleryItems).join('');
+
+galleryRef.insertAdjacentHTML('beforeend', galleryWrapper);
+
+const lightBox = new SimpleLightbox('div.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250',
+    captionPosition: 'bottom',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const throttle = require('lodash.throttle');
 
+player.on('play', function () {});
+
+player.on(
+    'timeupdate',
+    throttle(function (data) {
+        localStorage.setItem('videoplayer-current-time', Math.floor(data.seconds));
+    }, 1000),
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).catch(function (error) {
+    switch (error.name) {
+        default:
+            break;
+    }
+});
